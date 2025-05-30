91d928c15afa70ff769f90085b025a4d350a2df5
Julio Cesar de Jesus Bonfim <159816707+Juliocesarsenai@users.noreply.github.com>
2025-05-29 22:08:44 -0300

    Add files via upload

---
+++ Calculadora_React/calculadora/src/components/Button/index.js
@@ -0,0 +1,13 @@
+//calculadora\src\components\Button\index.js
+import { ButtonContainer } from "./styles";
+
+const Button = ({label,onClick}) => {
+  return (
+    <ButtonContainer onClick={onClick} type="button">
+      {label}
+      </ButtonContainer>   
+   
+  );
+}
+
+export default Button;
---
+++ Calculadora_React/calculadora/src/components/Input/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+
+export const InputContainer= styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #4169E1;
+
+    display:flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family:'Roboto';
+    margin: 0;
+
+    input {
+    width: 100%;
+    height:75px;
+    background-color: #4169E1;
+
+    border-radius:5px;
+    border:2px solid #FFF;
+
+    flex-direction:column;
+    align-items: flex-end;
+    
+    padding:0 10px;
+    font-size: 24px;
+    font-family:'Roboto';
+    color:#FFFFFF;
+    }
+`;
